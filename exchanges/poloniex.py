775e4b55a9b228ed9b8797c0fe985a75c5430d31
Anthony Albertina <alberta1@sunyit.edu>
2018-01-04 14:33:14 -0800

    added exchanges

---
+++ exchanges/poloniex.py
@@ -0,0 +1,80 @@
+from exchanges.exchange import Exchange
+from env import poloniex
+
+import requests
+
+class Poloniex(Exchange):
+
+    __apikey = ""
+    __apisecret = ""
+
+    def __init__(self):
+        self.__apikey = poloniex['apikey']
+        self.__apisecret = poloniex['apisecret']
+
+    # Private API
+    def getBalance(self):
+        pass
+
+    def getOpenOrders(self):
+        pass
+
+    def getDepositAddress(self):
+        pass
+
+    def getTradeHistory(self):
+        pass
+
+    def getTransactions(self):
+        pass
+
+    def submitTrade(self):
+        pass
+
+    def cancelTrade(self):
+        pass
+
+    def submitTip(self):
+        pass
+
+    def submitWithdraw(self):
+        pass
+
+    def submitTransfer(self):
+        pass
+
+    # Public API
+    def getCurrencies(self):
+        response = requests.get('https://www.cryptopia.co.nz/api/GetCurrencies')
+        return(response.json())
+
+    def getTradePairs(self):
+        response = requests.get('https://www.cryptopia.co.nz/api/GetTradePairs')
+        return(response.json())
+
+    def getMarkets(self, market=0):
+        if market == 0:
+            response = requests.get('https://www.cryptopia.co.nz/api/GetMarkets')
+        else:
+            response = requests.get('https://www.cryptopia.co.nz/api/GetMarkets/'+market)
+        return(response.json())
+
+    def getMarket(self, market):
+        response = requests.get('https://www.cryptopia.co.nz/api/GetMarket/'+market)
+        return(response.json())
+
+    def getMarketHistory(self, market):
+        response = requests.get('https://www.cryptopia.co.nz/api/GetMarketHistory/'+market)
+        return(response.json())
+
+    def getMarketOrders(self):
+        pass
+
+    def getMarketOrderGropus(self):
+        pass
+
+    
+
+
+
+
--- trading-methods/volatility.py
+++ trading-methods/volatility.py
@@ -1,13 +1,17 @@
 import sys
-sys.path.append('../') # To keep file structure
+sys.path.append('../../') # To keep file structure
 
 from exchanges.exchange import Exchange
 from exchanges.cryptopia import Cryptopia
 
 def main():
     exchange1 = Cryptopia()
-    #exchange1.getapikey()
-    #cryptopiaExchange.getTemp()
-    #print(cryptopia)
+    result = exchange1.getMarket('LTC_BTC')
+    #print(result['Data'][0]['TradePairId'])
+    #print(len(result['Data']))
+    #print(result['Data'][497]['TradePairId'])
+    #print(result)
+    #for n in range(len(result['Data'])):
+    #    print(result['Data'][n]['TradePairId'])
 
 main()
